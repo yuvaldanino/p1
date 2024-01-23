5c9ad83906b9029a03a9e2906b3be77c16e959c3
Yuval Danino <daninoyuval@gmail.com>
2024-01-22 18:41:27 -0800

    phase 3 complete

--- sshell.c
+++ sshell.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <limits.h> // For PATH_MAX
 
 #define CMDLINE_MAX 512
 #define MAX_ARGS 10
@@ -46,9 +47,11 @@ void executeCommand(struct Command *command) {
                 waitpid(pid,&status, 0); 
                 fprintf(stdout, "Return status value for '%s': %d\n",command->argv[0], WEXITSTATUS(status));
         }else {    // pid is 0 -> child 
+                
+                // checking statement to ensure reading right arguments 
+                //fprintf(stdout, "Argument is '%s'\n",command->argv[0]);
+               
                 //execvp -> number of arguments is not known 
-                fprintf(stdout, "Argument is '%s'\n",command->argv[0]);
-
                 execvp(command->argv[0], command->argv);
 
                 // if fails and doesnt execute 
@@ -60,10 +63,47 @@ void executeCommand(struct Command *command) {
 }
 
 void handleBuiltInCommands(struct Command *command, int *shouldContinue) {
-        if (command->argc > 0 && strcmp(command->argv[0], "exit") == 0) {
+        
+        //error wrong size
+        if(command->argc <= 0){
+                perror("no arguments");
+                exit(1);               
+        }
+
+        if (strcmp(command->argv[0], "exit") == 0){
+                
                 fprintf(stderr, "Bye...\n");
-                *shouldContinue = 0;
-    }
+                *shouldContinue = 0; 
+
+        }else if(strcmp(command->argv[0], "pwd") == 0){
+                //pwd
+                // declare char for max path size 
+                char cwd[PATH_MAX];
+
+                //checks if there exists a path 
+                if( getcwd(cwd, sizeof(cwd)) != NULL ){
+                        //prints cwd if not NULL
+                        printf("%s\n", cwd);   
+                }else{
+                        perror("getcwd error");
+                
+                }
+
+        }else if(strcmp(command->argv[0], "cd") == 0){
+                // cd
+
+                // ensure cd has argument 
+                if(command->argc > 1){
+                        //chdir returns 0 if worked correctly, error for other values 
+                      if(chdir(command-> argv[1]) != 0 ){
+                        // if error chdir
+                        perror("chdir");
+                      }  
+                }else{
+                        // no arguments for cd 
+                      fprintf(stderr, "cd: missing argument\n");  
+                }
+        }
 }
 
 
@@ -87,15 +127,14 @@ int main(void){
 
                 // parse the command line 
                 parseCommand(cmd, &command);
+                // handle the built in commands exit, pwd, cd 
                 handleBuiltInCommands(&command, &keepGoing);
 
+                // execute commands 
                 if (keepGoing){
                         executeCommand(&command);
                 }
                 
-
-
-               
                 /* Get command line */
                 //fgets(cmd, CMDLINE_MAX, stdin);
 
